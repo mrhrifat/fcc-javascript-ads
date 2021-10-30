0ae85181402de9ee7862cb6e4d2137c68dc3782f
Rakibul Hassan Rifat <mrhrifat383759@gmail.com>
2021-10-30 14:14:19 +0600

    Add code 3 to 5

---
+++ FunctionalProgramming/05_PassArguments.js
@@ -0,0 +1,11 @@
+// The global variable
+let fixedValue = 4;
+
+// Only change code below this line
+function incrementer(hasIncrement) {
+
+    return hasIncrement + 1
+    // Only change code above this line
+}
+console.log(incrementer(fixedValue))
+console.log(fixedValue)
