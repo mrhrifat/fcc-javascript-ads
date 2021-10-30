c085c5c0cfcb23760683df357c4b1c55a6308e15
Rakibul Hassan Rifat <mrhrifat383759@gmail.com>
2021-10-30 20:34:13 +0600

    Upload Problem & Solution 21-24 at Funcational Programming

---
+++ FunctionalProgramming/23_SomeMethod.js
@@ -0,0 +1,8 @@
+function checkPositive(arr) {
+    // Only change code below this line
+    return arr.some(v => v > 0)
+
+    // Only change code above this line
+}
+
+console.log(checkPositive([1, 2, 3, -4, 5]))
