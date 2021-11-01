071a983a270abfd103aa53b800f8b319ce922f7d
Rakibul Hassan Rifat <mrhrifat383759@gmail.com>
2021-11-02 00:26:43 +0600

    Upload IAS Problem & Solution 20-21 Completed

---
+++ IntermediateAlgorithmScripting/20_MakeAPerson.js
@@ -0,0 +1,62 @@
+var Person = function (firstAndLast) {
+    var fullName = firstAndLast
+
+    this.getFirstName = function () {
+        return fullName.split(" ")[0]
+    };
+
+    this.getLastName = function () {
+        return fullName.split(" ")[1]
+    };
+
+    this.getFullName = function () {
+        return fullName
+    };
+
+    this.setFirstName = function (name) {
+        fullName = name + " " + fullName.split(" ")[1]
+    };
+    this.setLastName = function (name) {
+        fullName = fullName.split(" ")[0] + " " + name
+    };
+    this.setFullNameName = function (name) {
+        fullName = name
+    };
+};
+
+const bob = new Person('Bob Ross');
+console.log(bob.getFullName())
+console.log(bob.getFirstName())
+console.log(bob.getLastName())
+
+// var Person = function(firstAndLast) {
+//     var fullName = firstAndLast;
+  
+//     this.getFirstName = function() {
+//       return fullName.split(" ")[0];
+//     };
+  
+//     this.getLastName = function() {
+//       return fullName.split(" ")[1];
+//     };
+  
+//     this.getFullName = function() {
+//       return fullName;
+//     };
+  
+//     this.setFirstName = function(name) {
+//       fullName = name + " " + fullName.split(" ")[1];
+//     };
+  
+//     this.setLastName = function(name) {
+//       fullName = fullName.split(" ")[0] + " " + name;
+//     };
+  
+//     this.setFullName = function(name) {
+//       fullName = name;
+//     };
+//   };
+  
+//   var bob = new Person("Bob Ross");
+//   bob.getFullName();
+  
